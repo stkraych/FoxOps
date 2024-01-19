ce475a16be8c63a180297c484d2d4f1801c208e7
Stella Kraycheva <st.kraycheva@gmail.com>
2024-01-19 23:16:50 +0200

    [121] - remove terraform version

--- terraform/deploy/provider.tf
+++ terraform/deploy/provider.tf
@@ -1,15 +1,13 @@
 terraform {
-  required_version = "1.6.6"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
     }
   }
 
     backend "s3" {
-    bucket         	   = "state-bucket-9203143-4"
+    bucket         	   = "state-bucket-9203143-6"
     key              	   = "terraform.tfstate"
     region         	   = "us-east-1"
     encrypt        	   = true
