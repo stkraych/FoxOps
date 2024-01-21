de591c771442eafd65cd45d564d3419e9f5d2d04
Stella Kraycheva <st.kraycheva@gmail.com>
2024-01-21 20:46:46 +0200

    [131] - add autoscalling in destroy-flow

--- terraform/autoscalling/provider.tf
+++ terraform/autoscalling/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
     backend "s3" {
-    bucket         	   = "autoscalling-bucket-23482-7"
+    bucket         	   = "autoscalling-bucket-23482-8"
     key              	   = "autoscalling/terraform.tfstate"
     region         	   = "us-east-1"
     encrypt        	   = true
