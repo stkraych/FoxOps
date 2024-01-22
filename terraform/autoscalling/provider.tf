2fb231f8c725855d98c755afc636445cc7c4a36f
Stella Kraycheva <st.kraycheva@gmail.com>
2024-01-22 20:20:29 +0200

    [140] - test the whole structure

--- terraform/autoscalling/provider.tf
+++ terraform/autoscalling/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
     backend "s3" {
-    bucket         	   = "autoscalling-bucket-23482-15"
+    bucket         	   = "autoscalling-bucket-23482-16"
     key              	   = "autoscalling/terraform.tfstate"
     region         	   = "us-east-1"
     encrypt        	   = true
